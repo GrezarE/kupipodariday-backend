dd987a8a66a50b1eec60c6a9cac299f1f9867a72
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2022-11-27 05:25:32 +0300

    docker adding

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
             ref: DEPLOY_REF,
             repo: DEPLOY_REPO,
             path: DEPLOY_PATH,
-            'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+            'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current`,
             'post-deploy': 'docker compose up --build -d',
         },
     },
